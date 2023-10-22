3930cb6d19dd8a61b0f43b993d4f1be6606d61c4
Clemens Näther <naether.clemens@gmail.com>
2023-10-22 11:43:22 +0200

    added input stream for files

--- Lexer.java
+++ Lexer.java
@@ -1,10 +1,27 @@
+import java.io.InputStream;
+
 public class Lexer {
+
+    private InputStream in;
+    private int state;
+
+    public Lexer(InputStream inputStream) {
+        state = 0;
+        in = inputStream;
+    }
+
+    public void printFile() throws Exception {
+        int c;
+        while ((c = in.read()) != -1) {
+            System.out.print((char) c);
+        }
+    }
 
     // Zeichenklassen
     // 0: Sonderzeichen 1: Ziffern 2: Buchstaben
     // 3: : 4: = 5: < 6: > 7: sonstige
 
-    static char[] zeichenklassen = {
+    static int[] zeichenklassen = {
             /* 0 1 2 3 4 5 6 7 8 9 A B C D E F */
             /* 0 */ 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
             /* 10 */ 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
@@ -84,19 +101,26 @@ public class Lexer {
 
     // Automatentabelle
     State[] automatentabelle1 = {
-        /*          0   1   2   3   4   5   6   7      */
-        /* z0 */    new slb(0), new slb(1), new sgl(2), new sl(3), new slb(0), new sl(4), new sl(5), new b(0),
-        /* z1 */    new b(0), new sl(1), new b(0), new b(0), new b(0), new b(0), new b(0), new b(0), new b(0),
-        /* z2 */    new b(0), new sl(2), new sgl(2), new b(0), new b(0), new b(0), new b(0), new b(0), new b(0),
-        /* z3 */    new b(0), new b(0), new b(0), new b(0), new sl(6), new b(0), new b(0), new b(0), new b(0),
-        /* z4 */    new b(0), new b(0), new b(0), new b(0), new sl(7), new b(0), new b(0), new b(0), new b(0),
-        /* z5 */    new b(0), new b(0), new b(0), new b(0), new sl(8), new b(0), new b(0), new b(0), new b(0),
-        /* z6 */    new b(0), new b(0), new b(0), new b(0), new b(0), new b(0), new b(0), new b(0), new b(0),
-        /* z7 */    new b(0), new b(0), new b(0), new b(0), new b(0), new b(0), new b(0), new b(0), new b(0),
-        /* z8 */    new b(0), new b(0), new b(0), new b(0), new b(0), new b(0), new b(0), new b(0), new b(0)
-    }
+            /* 0 1 2 3 4 5 6 7 */
+            /* z0 */ new slb(0), new slb(1), new sgl(2), new sl(3), new slb(0), new sl(4), new sl(5), new b(0),
+            /* z1 */ new b(0), new sl(1), new b(0), new b(0), new b(0), new b(0), new b(0), new b(0), new b(0),
+            /* z2 */ new b(0), new sl(2), new sgl(2), new b(0), new b(0), new b(0), new b(0), new b(0), new b(0),
+            /* z3 */ new b(0), new b(0), new b(0), new b(0), new sl(6), new b(0), new b(0), new b(0), new b(0),
+            /* z4 */ new b(0), new b(0), new b(0), new b(0), new sl(7), new b(0), new b(0), new b(0), new b(0),
+            /* z5 */ new b(0), new b(0), new b(0), new b(0), new sl(8), new b(0), new b(0), new b(0), new b(0),
+            /* z6 */ new b(0), new b(0), new b(0), new b(0), new b(0), new b(0), new b(0), new b(0), new b(0),
+            /* z7 */ new b(0), new b(0), new b(0), new b(0), new b(0), new b(0), new b(0), new b(0), new b(0),
+            /* z8 */ new b(0), new b(0), new b(0), new b(0), new b(0), new b(0), new b(0), new b(0), new b(0)
+    };
 
-    public static void main(String args[]) {
-
+    public static void main(String args[]) throws Exception {
+        String path = args[0];
+        InputStream inputStream = Lexer.class.getResourceAsStream(path);
+        if (inputStream == null) {
+            System.out.println("File not found");
+            return;
+        }
+        Lexer lexer = new Lexer(inputStream);
+        lexer.printFile();
     }
 }
