7fb8fec9ea0e08caa5608c274e4d6de5fbe381eb
Clemens Näther <s85426@isys13.informatik.htw-dresden.de>
2023-10-26 12:55:06 +0200

    Praktikum 26.10.2023

--- Lexer.java
+++ Lexer.java
@@ -4,32 +4,142 @@ import java.io.InputStreamReader;
 import java.nio.Buffer;
 
 enum TokenType {
-    IDENTIFIER, NUMBER, PLUS, MINUS, TIMES, SLASH, ODD, EQL, NEQ, LSS, LEQ, GTR, GEQ, LPAREN, RPAREN, COMMA, SEMICOLON,
-    PERIOD, BECOMES, BEGIN, END, IF, THEN, WHILE, DO, CALL, CONST, VAR, PROCEDURE, WRITE, READ;
-}
-
-class Token {
-    public TokenType type;
-    public String value;
-
-    public Token(TokenType type, String value) {
-        this.type = type;
-        this.value = value;
-    }
+    NIL, SYM, NUM, IDENT;
 }
 
 public class Lexer {
     private InputStream in;
+    private int currentState;
+    private State nextState;
     private BufferedReader reader;
     private char currentChar;
-    private StringBuilder currentTokenValue;
+    private String currentTokenValue;
+    private Token t;
+    private String currentToken;
 
     public Lexer(InputStream inputStream) throws Exception {
         in = inputStream;
         reader = new BufferedReader(new InputStreamReader(in));
-        currentChar = (char) reader.read();
-        currentTokenValue = new StringBuilder();
+        currentTokenValue = new String();
+        currentToken = new String();
     }
+
+    class Token {
+        private TokenType type;
+        private String value;
+        private int line, col;
+
+        public Token(TokenType type, String value) {
+            this.type = type;
+            this.value = value;
+        }
+    }
+
+    abstract class State {
+        public int state;
+
+        public State(int State) {
+            this.state = state;
+        }
+
+        public int getState() {
+            return state;
+        }
+
+        public abstract void action();
+    }
+
+    void fl() {
+        try {
+            currentChar = (char) reader.read();
+        } catch (Exception e) {
+            System.out.println("Fehler: " + e);
+        }
+    }
+
+    void fs() {
+        currentTokenValue += currentChar;
+    }
+
+    void fb() {
+        switch(currentState) {
+            case 3: //:
+            case 4: //<
+            case 5: //>
+            case 6: //=
+            case 7: //<=
+            case 8: //>=
+            case 0: //sonstige
+                t = new Token(TokenType.SYM, currentTokenValue.toString());
+                break;
+            case 1: //Zahl
+                t = new Token(TokenType.NUM ,currentTokenValue.toString());
+                break;
+            case 2: //Buchstabe
+                t = new Token(TokenType.IDENT, currentTokenValue.toString());
+                break;
+        }
+    }
+
+    class l extends State {
+        public l(int state) {
+            super(state);
+        }
+
+        // lesen
+        public void action() {
+            fl();
+        }
+    }
+
+    class sl extends State {
+        public sl(int state) {
+            super(state);
+        }
+
+        // speichern und lesen
+        public void action() {
+            fs();
+            fl();
+        }
+    }
+
+    class sgl extends State {
+        public sgl(int state) {
+            super(state);
+        }
+
+        // speichern, groß und lesen
+        public void action() {
+            currentChar = Character.toUpperCase(currentChar);
+            fs();
+            fl();
+        }
+    }
+
+    class slb extends State {
+        public slb(int state) {
+            super(state);
+        }
+
+        // speichern, lesen und beenden
+        public void action() {
+            fs();
+            fl();
+            fb();
+        }
+    }
+
+    class b extends State {
+        public b(int state) {
+            super(state);
+        }
+
+        public void action() {
+            fb();
+        }
+    }
+
 
     // Zeichenklassen
     // 0: Sonderzeichen 1: Ziffern 2: Buchstaben
@@ -46,15 +156,32 @@ public class Lexer {
             /* 70 */ 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0
     };
 
+    //Automatentabelle
+    State[][] automatentabelle = {
+            /*          0           1            2          3           4           5       6         7 */
+            /* z0 */ {new slb(0), new sl(1), new sgl(2), new sl(3), new slb(0), new sl(4), new sl(5), new l(0)},
+            /* z1 */ {new b(0), new sl(1), new b(0), new b(0), new b(0), new b(0), new b(0), new b(0), new b(0)},
+            /* z2 */ {new b(0), new sl(2), new sgl(2), new b(0), new b(0), new b(0), new b(0), new b(0), new b(0)},
+            /* z3 */ {new b(0), new b(0), new b(0), new b(0), new sl(6), new b(0), new b(0), new b(0), new b(0)},
+            /* z4 */ {new b(0), new b(0), new b(0), new b(0), new sl(7), new b(0), new b(0), new b(0), new b(0)},
+            /* z5 */ {new b(0), new b(0), new b(0), new b(0), new sl(8), new b(0), new b(0), new b(0), new b(0)},
+            /* z6 */ {new b(0), new b(0), new b(0), new b(0), new b(0), new b(0), new b(0), new b(0), new b(0)},
+            /* z7 */ {new b(0), new b(0), new b(0), new b(0), new b(0), new b(0), new b(0), new b(0), new b(0)},
+            /* z8 */ {new b(0), new b(0), new b(0), new b(0), new b(0), new b(0), new b(0), new b(0), new b(0)}
+    };
+
     public Token getNextToken() throws Exception {
         int zeichenklasse;
-        int nextState;
+        currentTokenValue = "";
+        currentToken = "";
         while (currentChar != -1) {
             zeichenklasse = zeichenklassen[currentChar];
-            nextState = 
+            nextState = automatentabelle[currentState][zeichenklasse];
+            nextState.action();
+            currentState = nextState.getState();
         }
-    
-    }
+        return t;
+    };
 
     public static void main(String args[]) throws Exception {
         String path = args[0];
@@ -63,5 +190,11 @@ public class Lexer {
             System.out.println("File not found");
             return;
         }
+        Lexer lexer = new Lexer(inputStream);
+        Token token;
+        while ((token = lexer.getNextToken()) != null) {
+            System.out.println(token);
+        }
+
     }
 }
--- src/Lex.java
+++ src/Lex.java
@@ -3,8 +3,7 @@ import java.io.FileReader;
 import java.io.IOException;
 
 enum TokenType {
-    IDENTIFIER, NUMBER, PLUS, MINUS, TIMES, SLASH, ODD, EQL, NEQ, LSS, LEQ, GTR, GEQ, LPAREN, RPAREN, COMMA, SEMICOLON,
-    PERIOD, BECOMES, BEGIN, END, IF, THEN, WHILE, DO, CALL, CONST, VAR, PROCEDURE, WRITE, READ;
+    IDENTIFIER, NUMBER, PLUS, MINUS, 
 }
 
 class Token {
@@ -37,13 +36,12 @@ public class Lex {
             } else if (Character.isDigit(currentChar)) {
                 return readNumber();
             } else {
-                // Handle operators, punctuation, and other symbols
+                // Handle operators
                 switch (currentChar) {
                     case '+':
                         currentChar = (char) reader.read();
                         return new Token(TokenType.PLUS, "+");
-                    // Add cases for other operators and symbols
-                    // ...
+                    // Add cases
                     default:
                         // Handle unknown characters or report an error
                         currentChar = (char) reader.read();
@@ -67,20 +65,6 @@ public class Lex {
         }
 
         String tokenValue = currentTokenValue.toString();
-        switch (tokenValue) {
-            case "BEGIN":
-                return new Token(TokenType.BEGIN, tokenValue);
-            case "END":
-                return new Token(TokenType.END, tokenValue);
-            case "IF":
-                return new Token(TokenType.IF, tokenValue);
-            case "THEN":
-                return new Token(TokenType.THEN, tokenValue);
-            // Add cases for other keywords
-            // ...
-            default:
-                return new Token(TokenType.IDENTIFIER, tokenValue);
-        }
     }
 
     private Token readNumber() throws IOException {
@@ -97,7 +81,7 @@ public class Lex {
     }
 
     public static void main(String[] args) throws IOException {
-        Lex lexer = new Lex("../PL0-Compiler/src/fakult.pl0");
+        Lex lexer = new Lex("../src/fakult.pl0");
         Token token;
         while ((token = lexer.getNextToken()) != null) {
             System.out.println("Token: " + token.type + ", Value: " + token.value);
