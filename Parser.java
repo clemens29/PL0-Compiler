9b3e4625d9de7e237783862b910f0872e439aa6c
Clemens Näther <s85426@isys13.informatik.htw-dresden.de>
2023-11-09 11:36:50 +0100

    added Edge classes

--- Parser.java
+++ Parser.java
@@ -5,8 +5,40 @@ import java.io.InputStreamReader;
 
 public class Parser extends Lexer{
 
-    public class Bogen {
+    public class Edge {
+        int next;
+        int alt;
+        public Edge(int n, int a) {
+            next = n;
+            alt = a;
+        }
+        public Edge() {}
+    }
 
+    public class EdgeNil extends Edge {}
+
+    public class EdgeSymbol extends Edge {
+        int symbol;
+        public EdgeSymbol(int s, int n, int a) {
+            super(n,a);
+            symbol = s;
+        }
+    }
+
+    public class EdgeToken extends Edge {
+        Token token;
+        public EdgeToken(Token t, int n, int a) {
+            super(n,a);
+            token = t;
+        }
+    }   
+
+    public class EdgeGraph extends Edge {
+        Edge graph;
+        public EdgeGraph(Edge g, int n, int a) {
+            super(n,a);
+            graph = g;
+        }
     }
 
     public Parser() {
