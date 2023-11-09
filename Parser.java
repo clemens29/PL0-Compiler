bc65d9cff4e53c2ebd6d063932d2561066d4830e
Clemens Näther <s85426@isys13.informatik.htw-dresden.de>
2023-11-09 13:00:18 +0100

    added graphs (not done)

--- Parser.java
+++ Parser.java
@@ -5,6 +5,8 @@ import java.io.InputStreamReader;
 
 public class Parser extends Lexer{
 
+    Lexer lexer = new Lexer();
+
     public class Edge {
         int next;
         int alt;
@@ -15,7 +17,11 @@ public class Parser extends Lexer{
         public Edge() {}
     }
 
-    public class EdgeNil extends Edge {}
+    public class EdgeNil extends Edge {
+        public EdgeNil(int n, int a) {
+            super(n,a);
+        }
+    }
 
     public class EdgeSymbol extends Edge {
         int symbol;
@@ -35,19 +41,81 @@ public class Parser extends Lexer{
 
     public class EdgeGraph extends Edge {
         Edge graph;
-        public EdgeGraph(Edge g, int n, int a) {
+        public EdgeGraph(Edge[] g, int n, int a) {
             super(n,a);
             graph = g;
         }
     }
 
-    public Parser() {
-
+    public class EdgeEnd extends Edge {
+        public EdgeEnd(int n, int a) {
+            super(n,a);
+        }
     }
-    
+
+    Edge[] gProgramm;
+    Edge[] gBlock;
+    Edge[] gExpr;
+    Edge[] gTerm;
+    Edge[] gStatement;
+    Edge[] gFact;
+    Edge[] gCondition;
+
+    public Parser() {
+        gProgramm = new Edge[] {
+            new EdgeGraph(gBlock, 1, 0),
+            new EdgeSymbol((int)'.', 2, 0),
+            new EdgeEnd(0, 0)
+        };
+
+        gBlock = new Edge[] {
+
+        };
+
+        gExpr = new Edge[] {
+            new EdgeSymbol((int)'-', 2, 1),
+            new EdgeGraph(gTerm, 3, 0),
+            new EdgeNil(4,5),
+            new EdgeSymbol((int)'+', 6, 5),
+            new EdgeSymbol((int)'-', 6, 7),
+            new EdgeGraph(gTerm, 3, 0),
+            new EdgeNil(8, 4),
+            new EdgeEnd(0, 0)
+        };
+
+        gTerm = new Edge[] {
+            new EdgeGraph(gFact, 1, 0),
+            new EdgeNil(2,3),
+            new EdgeSymbol((int)'*', 4, 0),
+            new EdgeSymbol((int)'/', 4, 0),
+            new EdgeGraph(gFact, 1, 0),
+            new EdgeNil(7, 4),
+            new EdgeEnd(0, 0)
+        };
+
+        gStatement = new Edge[] {
+
+        };
+
+        gFact = new Edge[] {
+            new EdgeToken(lexer.new Token(TokenType.IDENT, null), 5, 1),
+            new EdgeToken(lexer.new Token(TokenType.NUM, null), 5, 2),
+            new EdgeSymbol((int)'(', 3, 0),
+            new EdgeGraph(gExpr, 4, 0),
+            new EdgeSymbol((int)'(', 5, 0),
+            new EdgeEnd(0, 0)
+        };
+
+        gCondition = new Edge[] {
+
+        };
+        
+    }
 
 
     
 
-   
+    public static void main(String args[]) {
+        
+    }
 }
